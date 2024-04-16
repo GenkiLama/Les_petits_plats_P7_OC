f01f065c05eb3ed96e6fc53b32b9d9e189a4cdad
QuentinTR <97187394+GenkiLama@users.noreply.github.com>
2024-04-16 19:21:25 +0000

    comments

--- main.js
+++ main.js
@@ -6,25 +6,25 @@ import displayCount from './components/count'
 import {displayFilterTags , displayAppFilterTags, displayUstFilterTags , getUniqIngList , getUniqAppList , getUniqUstList} from './components/dropdown'
 import { renderIngTags , renderAppTags , renderUstTags } from './components/tag'
 
+//Affichage du header
 document.querySelector('header').innerHTML= header()
+
+//Gestion de l'input utilisateur dans la recherche principale
 const searchInput = document.querySelector('#search-recipe')
 searchInput.addEventListener('input',function(){
         const inputValue = searchInput.value
+        //Si la longueur de la chaine de caractère est inférieure à 3, on cache la croix de suppression
         if(inputValue.length<3){
             document.querySelector('.clearCross').style.display='none'
         }else{
             document.querySelector('.clearCross').style.display='block'
         }
+        //On setup le filtre de données en fonction de la valeur de l'input
         setInputFilter(inputValue.toLowerCase().normalize("NFD").trim().replace(/[\u0300-\u036f]/g, "").replace(/'/g, " "))
         init()
 })
-document.querySelectorAll('.filterButton').forEach(btn=>{
-    btn.addEventListener('click',function(e){
-        document.getElementById(`${e.target.id}_unfoldable`).classList.toggle('unfold')
-        document.querySelector(`.filtrIcon_${e.target.id}`).classList.toggle('rotate')
-    })
-})
 
+//Gestion de la suppression de l'input utilisateur
 document.querySelector('.clearCross').addEventListener('click',function(){
     searchInput.value = ""
     document.querySelector('.clearCross').style.display='none'
@@ -33,10 +33,21 @@ document.querySelector('.clearCross').addEventListener('click',function(){
     init()
 })
 
+//Gestion du déroulé des dropdowns + animation V
+document.querySelectorAll('.filterButton').forEach(btn=>{
+    btn.addEventListener('click',function(e){
+        document.getElementById(`${e.target.id}_unfoldable`).classList.toggle('unfold')
+        document.querySelector(`.filtrIcon_${e.target.id}`).classList.toggle('rotate')
+    })
+})
+
+
+
 export let inputIngredientValue =''
 export let inputApplianceValue =''
 export let inputUstensilsValue =''
 
+//Affichage ou non de la croix de suppression de l'input utilisateur dans les dropdowns
 const filterIngInput = document.getElementById('filterIngInput')
 filterIngInput.addEventListener('input', function() {
     const { value } = filterIngInput;
@@ -61,7 +72,9 @@ filterUstInput.addEventListener('input',function(){
 
 
 function init(){
+    //Affichage des recettes
     document.querySelector('section').innerHTML= renderCards().join(' ')
+    //Affichage des filtres
     document.querySelector('.filterList').innerHTML= displayFilterTags()
     document.querySelectorAll('.ingFilter').forEach(ing=>{
         ing.addEventListener('click',function(e){
@@ -83,9 +96,11 @@ function init(){
             init()
         })
     })
+    //Affichage du nombre de recettes
     displayCount(getData().length, searchInput.value)
-
+    //Affichage des tags
     document.querySelector('.ingTagsContainer').innerHTML = renderIngTags()
+    //Gestion des clics sur les tags
     document.querySelectorAll('.ingTag').forEach(tag=>{
         tag.addEventListener('click', function(e){
             const targetID = e.target.id.replace('tag-','')
